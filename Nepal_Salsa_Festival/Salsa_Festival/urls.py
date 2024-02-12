aa302200c39b09d7814e4eeeba3802956dc75464
radhamagar <radhabudhamagar8@gmail.com>
2024-02-12 22:34:33 +0545

    Schedule and Contact Pages

--- Nepal_Salsa_Festival/Salsa_Festival/urls.py
+++ Nepal_Salsa_Festival/Salsa_Festival/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('schedule', views.schedule, name='schedule'),
     path('', include('authentication.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Nepal_Salsa_Festival/Salsa_Festival/views.py
+++ Nepal_Salsa_Festival/Salsa_Festival/views.py
@@ -6,6 +6,14 @@ from django.shortcuts import redirect
 # Create your views here.
 def home(request):
     return render(request, template_name='home.html')
+def contact(request):
+    context = {}
+    return render(request, "contact.html", context)
+
+def schedule(request):
+    context = {}
+    return render(request, "schedule.html", context)
+
 
  
 
